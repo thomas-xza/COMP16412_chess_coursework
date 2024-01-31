33f806b42730707879942dc52d6c68a09d96454c
thomas <thomas.prior@student.manchester.ac.uk>
2024-01-31 15:46:53 +0000

    Hopefully covered all edge cases within isLegitMove() for Bishop

--- chess-project/src/main/chess/Bishop.java
+++ chess-project/src/main/chess/Bishop.java
@@ -25,30 +25,61 @@ class Bishop extends Piece{
 
     //    isLegitMove needs to check that:
     //      - move is legal within rules of chess
-    //      - move does not land on another piece
     //      - move is not zero
-
+    //      - move does not land on piece of same player
+    
+    //    Note that some code may be reusable across piece types - put
+    //    methods in Piece.java?
 
     
-	@Override
-	boolean isLegitMove(int i0, int j0, int i1, int j1) {
+    @Override
+    boolean isLegitMove(int i0, int j0, int i1, int j1) {
 
-	    int move_i = Math.abs(i0 - i1);
+	int move_i = Math.abs(i0 - i1);
 
-	    int move_j = Math.abs(i0 - i1);
+	int move_j = Math.abs(i0 - i1);
 
-	    if ( (move_i + move_j != 0) &&
-		 (move_i == move_j) &&
-		 Board.hasPiece(i1, j1) == false
-		 ) {
-		
-		return true;
-		
-	    } else {
-		
-		return false;
-		
-	    }
-	    
+	boolean legal_for_piece_type = false;
+
+	boolean target_exists = Board.hasPiece(i1, j1);
+
+	boolean target_exists_opponent = false;
+
+	//  Set variable if moving into opponent's piece.
+
+	if (target_exists == true &&
+	    (Board.getPiece(i0,j0).PieceColour !=
+	     Board.getPiece(i1,j1).PieceColour) ) {
+
+	    target_exists_opponent = true;
+
 	}
+
+	//  Set variable if move legal for this piece type.
+
+	if (move_i == move_j &&
+	    move_i != 0 &&
+	    move_j != 0) {
+
+	    legal_for_piece_type = true;
+
+	}
+
+	//  Make deductions based on facts available, return result.
+
+	if ( legal_for_piece_type == true &&
+	     (target_exists == false ||
+	      target_exists_opponent == true) )
+	     ) {
+		
+	    return true;
+		
+	} else {
+		
+	    return false;
+		     
+	}
+	    
+	
+    }
 }
